42f3d03195e381f414ef208536fc22fc46429f27
Max Lyubchenko <00eagermax00@gmail.com>
2024-10-23 17:59:22 +0300

    Add components Categories, Sort, TopBar & in app/page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title } from '@/components/shared';
+import { Container, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
   return (
@@ -6,6 +6,7 @@ export default function Home() {
       <Container className='mt-10'>
         <Title text='Все пиццы' size='lg' className='font-extrabold mt-7' />
       </Container>
+      <TopBar />
     </>
   );
 }
---
+++ components/shared/index.ts
@@ -0,0 +1,6 @@
+export { Header } from '@/components/shared/header';
+export { Container } from '@/components/shared/container';
+export { Title } from '@/components/shared/title';
+export { Categories } from '@/components/shared/categories';
+export { Sort } from '@/components/shared/sort';
+export { TopBar } from '@/components/shared/top-bar';
